91f518edab8db963175d239aafe32467e113fbbe
ModeYapu <1207565967@qq.com>
2018-01-29 17:51:31 +0800

    j

--- Encryption/Program.cs
+++ Encryption/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -223,6 +221,18 @@ namespace Encryption
             }
 
             return hash;
+        }
+    }
+
+    public class pbkdf2
+    {
+        private static byte[] PBKDF2(string password, byte[] salt, int iterations, int outputBytes)
+        {
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt))
+            {
+                pbkdf2.IterationCount = iterations;
+                return pbkdf2.GetBytes(outputBytes);
+            }
         }
     }
 }
